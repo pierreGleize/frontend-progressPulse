340e9745014d281326433006975b745b35c00dd2
Alexandre <lassaignealexandre@gmail.com>
2024-12-09 14:37:30 +0100

    rajout des images

--- pages/ExercicesChoiceScreen.js
+++ pages/ExercicesChoiceScreen.js
@@ -38,35 +38,35 @@ export default function ExercicesChoicesScreen({ navigation }) {
         >
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require('../assets/Dos/rowingbarre.gif')}
           />
           <ExerciseBtn
             textButton="Traction"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/traction.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
           <ExerciseBtn
             textButton="Tirage hozizontale à la poulie"
-            image={require("../assets/rowing.png")}
+            image={require("../assets/Dos/rowingbarre.gif")}
           />
         </ScrollView>
         <View
