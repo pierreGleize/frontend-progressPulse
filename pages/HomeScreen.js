2f6eb7ee80a14158b65f3337a3ae37dbe9fd2850
Pierre Gleize <gleizepierre3gmail.com>
2024-12-09 14:35:38 +0100

    pierre design des pages

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Button from "../components/Button";
-import ExerciseBtn from "../components/ExerciseBtn";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function HomeScreen({ navigation }) {
   const handleAddWorkout = () => {
@@ -13,28 +13,57 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Button
-        background="#A3FD01"
-        borderColor="none"
-        textButton="Ajouter une séance"
-        textColor="black"
-        width="75%"
-        height={50}
-        onPress={handleAddWorkout}
-        isLinearGradiant={false}
-      />
-      <Button
-        background="#A3FD01"
-        borderColor="none"
-        textButton="Ma séance préfaite"
-        textColor="black"
-        width="75%"
-        height={50}
-        onPress={handleWorkoutNavigation}
-        isLinearGradiant={true}
-        colorsGradiant={["#3BC95F", "#1D632F"]}
-      />
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>Hello Pierre,</Text>
+        <Text style={styles.span}>prêt pour un nouvel entrainement ? </Text>
+        <View style={styles.infoContainer}>
+          <FontAwesome
+            name={"info-circle"}
+            size={30}
+            color={"#A3FD01"}
+            onPress={() => navigation.navigate("muscleGroup")}
+            style={styles.infoIcon}
+          />
+          <Text style={styles.textInfo}>
+            Crée ta séance et commence l'entrainement
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Button
+          background="#A3FD01"
+          borderColor="none"
+          textButton="Ajouter une séance"
+          textColor="black"
+          width={300}
+          height={50}
+          onPress={handleAddWorkout}
+          isLinearGradiant={false}
+        />
+        <Button
+          background="#A3FD01"
+          borderColor="none"
+          textButton="Ma séance préfaite"
+          textColor="black"
+          width={300}
+          height={50}
+          onPress={handleWorkoutNavigation}
+          isLinearGradiant={true}
+          colorsGradiant={["#3BC95F", "#1D632F"]}
+        />
+        <Button
+          background="#A3FD01"
+          borderColor="none"
+          textButton="Ma séance sur mesure"
+          textColor="black"
+          width={300}
+          height={50}
+          onPress={handleWorkoutNavigation}
+          isLinearGradiant={true}
+          colorsGradiant={["#3BC95F", "#1D632F"]}
+        />
+      </View>
     </View>
   );
 }
@@ -43,13 +72,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0D0D36",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingVertical: 50,
+    paddingHorizontal: 10,
   },
-  text: {
-    fontSize: 50,
-    padding: 50,
-    textAlign: "center",
+  topContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 600,
     color: "white",
+  },
+  infoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  span: {
+    color: "#A3FD01",
+    marginLeft: 5,
+  },
+  infoIcon: {
+    marginRight: 10,
+  },
+  textInfo: {
+    color: "white",
+  },
+  btnContainer: {
+    alignItems: "center",
+    flex: 2,
   },
 });
--- pages/MuscleGroupScreen.js
+++ pages/MuscleGroupScreen.js
@@ -1,48 +1,87 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Button from "../components/Button";
+import Underline from "../components/Underline";
 
 export default function MuscleGroupScreen({ navigation }) {
+  const handleFinish = () => {
+    navigation.navigate("workoutSummary", { backTo: "muscleGroup" });
+  };
+  const handleNavigateToExercice = (name) => {
+    navigation.navigate("exercicesChoices", { name: name });
+  };
+  const muscularGroup = [
+    "Quadriceps",
+    "Ischio",
+    "Biceps",
+    "Triceps",
+    "Épaules",
+    "Fessiers",
+    "Pectoraux",
+    "Dos",
+    "Abdominaux",
+    "Cardio",
+  ];
   return (
     <View style={styles.container}>
-      <FontAwesome
-        name={"chevron-left"}
-        size={30}
-        color={"#3BC95F"}
-        onPress={() => navigation.navigate("WorkoutType")}
-      />
-      <Text style={styles.text}>MuscleGroup Screen</Text>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate("exercicesChoices")}
-      >
-        <Text style={styles.btnText}>Biceps</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate("exercicesChoices")}
-      >
-        <Text style={styles.btnText}>Cardio</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate("exercicesChoices")}
-      >
-        <Text style={styles.btnText}>Triceps</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate("exercicesChoices")}
-      >
-        <Text style={styles.btnText}>Abdominaux</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{ backgroundColor: "green", padding: 15 }}
-        onPress={() =>
-          navigation.navigate("workoutSummary", { backTo: "muscleGroup" })
-        }
-      >
-        <Text style={styles.btnText}>Terminer</Text>
-      </TouchableOpacity>
+      <View style={styles.topContainer}>
+        <FontAwesome
+          name={"chevron-left"}
+          size={30}
+          color={"#3BC95F"}
+          onPress={() => navigation.navigate("WorkoutType")}
+        />
+        <View>
+          <Text style={styles.title}>Exercices </Text>
+          <Underline width={60} />
+        </View>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <FlatList
+          data={muscularGroup}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={2}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center",
+          }}
+          columnWrapperStyle={{
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+          renderItem={({ item }) => (
+            <Button
+              background="#A3FD01"
+              borderColor="none"
+              textButton={item}
+              textColor="white"
+              width={150}
+              height={50}
+              onPress={() => handleNavigateToExercice(item)}
+              isLinearGradiant={true}
+              colorsGradiant={["#3BC95F", "#1D632F"]}
+            />
+          )}
+        />
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.bottomText}>
+          Exercices sélectionnés : <Text style={styles.bottomSpan}>0</Text>
+        </Text>
+        <View style={styles.bottomBtnContainer}>
+          <Button
+            background="#A3FD01"
+            borderColor="none"
+            textButton="Terminer"
+            textColor="black"
+            width={150}
+            height={50}
+            onPress={handleFinish}
+            isLinearGradiant={false}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -50,27 +89,34 @@ export default function MuscleGroupScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "wheat",
+    backgroundColor: "#0D0D36",
+    paddingVertical: 50,
+    paddingHorizontal: 10,
+  },
+  topContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 32,
+    color: "white",
+    fontWeight: 600,
+  },
+  btnContainer: {
+    flex: 4,
+  },
+  bottomContainer: {
+    flex: 1,
+  },
+  bottomText: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  bottomSpan: {
+    fontWeight: "bold",
+  },
+  bottomBtnContainer: {
     alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 50,
-    padding: 50,
-    textAlign: "center",
-    color: "white",
-  },
-  btn: {
-    fontSize: 40,
-    borderRadius: 10,
-    backgroundColor: "black",
-    margin: 10,
-  },
-  btnText: {
-    fontSize: 30,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    textAlign: "center",
-    color: "white",
   },
 });
